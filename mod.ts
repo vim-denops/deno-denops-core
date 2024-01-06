0fb92ac16589d77097876d781f4ae2cfeebf3500
Alisue <lambdalisue@gmail.com>
2024-01-07 03:55:59 +0900

    :+1: Add `DenopsOptions` interface

--- mod.ts
+++ mod.ts
@@ -12,4 +12,10 @@
  */
 
 export { BatchError } from "./denops.ts";
-export type { Context, Denops, Dispatcher, Meta } from "./denops.ts";
+export type {
+  Context,
+  Denops,
+  DenopsOptions,
+  Dispatcher,
+  Meta,
+} from "./denops.ts";
